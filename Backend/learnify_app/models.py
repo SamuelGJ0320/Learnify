13ff71f66e5d6dedf20318ecd6f1c7c5069e50b8
MAXIMILIANO BUSTAMANTE <mbustamang@eafit.edu.co>
2025-03-09 21:34:51 -0500

    corrected minor reltaed name issue

--- Backend/learnify_app/models.py
+++ Backend/learnify_app/models.py
@@ -67,7 +67,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Order(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     total_price = models.DecimalField(max_digits=15, decimal_places=2)
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
