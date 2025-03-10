93d257aa42b7ce80c1b5068f06325def141d71fa
vvhornung <vvhornung@eafit.edu.co>
2025-03-09 21:14:07 -0500

    order model created

--- Backend/learnify_app/models.py
+++ Backend/learnify_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=15, decimal_places=2)
+    status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+
+  
