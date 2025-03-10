8a44230cdfce027c77d1da68c96c163c1929ef85
MAXIMILIANO BUSTAMANTE <mbustamang@eafit.edu.co>
2025-03-10 00:18:03 -0500

    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

--- Backend/Learnify/urls.py
+++ Backend/Learnify/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from learnify_app.urls import urlpatterns as learnify_app_urls #Importing the urls from the learnify_app to be used in the
+
+
+
+#learnify back end
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('dj_rest_auth.urls')), #Endpoint provided by dj_rest_aut
+    path('api/social/login/', include(learnify_app_urls)), #Endpoint provided by dj_rest_auth
 ]
+
--- Backend/learnify_app/models.py
+++ Backend/learnify_app/models.py
@@ -64,7 +64,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name", "username"]
 
     class Meta:
         verbose_name = "User"
