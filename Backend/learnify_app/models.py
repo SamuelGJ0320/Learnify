41085e7df35e6ca8ebe91e4efd10a20da8b74b55
MAXIMILIANO BUSTAMANTE <mbustamang@eafit.edu.co>
2025-03-09 22:41:52 -0500

    Created many to many fields between or5der and course

--- Backend/learnify_app/models.py
+++ Backend/learnify_app/models.py
@@ -70,6 +70,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     total_price = models.DecimalField(max_digits=15, decimal_places=2)
     status = models.CharField(max_length=50)
+    course = models.ManyToManyField("Course", related_name="orders")
     created_at = models.DateTimeField(auto_now_add=True)
     
 
