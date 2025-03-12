bd842ae5433f64db9b5911f856b0dda1a5dd2049
Juan Jara <jaracalle2041@gmail.com>
2025-03-12 09:12:53 -0500

    removed created at field from the user serializer

--- Backend/learnify_app/serializers.py
+++ Backend/learnify_app/serializers.py
@@ -13,7 +13,6 @@ class UserSerializer(ModelSerializer):
             "last_name",
             "profile_picture",
             "role",
-            "created_at",
             "password",
         ]
         extra_kwargs = {"password": {"write_only": True}}
