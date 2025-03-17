542f25fd94ac038fefaa62c2463ba80cf4340423
vale <vvhornung@eafit.edu.co>
2025-03-17 02:02:52 -0500

    Courses Page Created

---
+++ frontend/src/app/courses/page.jsx
@@ -0,0 +1,22 @@
+import Background from "@/components/BackgroundImage";
+import bg from "@public/home-background.svg";
+import React from "react";
+import { courses } from "@/lib/test";
+import CourseCard from "./components/CourseCard";
+import Link from "next/link";
+
+
+function Courses() {
+  return (
+    <>
+      <div className="flex flex-col w-full items-center justify-center page-wrapper">
+        {courses.map((course, index) => (
+          
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Courses;
