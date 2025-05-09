0c6be58515064172a324c44e9614ddf922e7948a
Juan Jara <jaracalle2041@gmail.com>
2025-05-09 11:10:24 -0500

    chore(API): add the pagination with size 10

--- Backend/Learnify/settings.py
+++ Backend/Learnify/settings.py
@@ -173,7 +173,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',  # Ensure this is correct
     ),
-    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 
